36922d6aedd8d15cc213de133e98d4a52f6cb00b
kamranabilov <kamrangab@code.edu.az>
2022-02-10 09:03:55 +0400

    AllTasksAdded

---
+++ 9fevral/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ClassTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //if (N < 50)
+            //{
+            //    for (int i = 1; i < N; i++)
+            //    if (i % 3 == 0) ;
+            //    {
+            //        count++;
+            //    }
+            //    {
+            //        Console.WriteLine(count);
+            //    } }
+            //      else if (50 < N && N <= 100) {
+
+            //    for (int i = 5; i <= N; i++)
+            //        if (i % 5 == 0)
+            //        {
+            //            count++;
+            //        }
+            //    Console.WriteLine(count); }
+
+            //else if (N > 100)
+
+            //{ for (int i = 8; i <= N; i++)
+
+            //    {
+            //        if (i % 8 == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //} Console.WriteLine(count);
+
+            //string[] words = { "toyota", "kia", "mazda", "passat" };
+            ////string[] newArr = new string[];
+
+            ////newArr[0] = "Toyota";
+            ////newArr[1] = "Kia";
+            ////newArr[2] = "Mazda";
+            ////newArr[3] = "Passat";
+            ////foreach (var item in newArr)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+
+            //foreach (string word in words)
+            //{
+            //    if (word.Length > 5)
+            //    {
+            //        Console.WriteLine(word);
+            //    }
+            //}
+            //int n, r, sum = 0;
+            //print("plase enter number: ");
+            //scan("%d", &n);
+            //while(n>0)
+            //{
+            //    r = n % 10;
+            //    sum = sum + r;
+            //    n = n / 10;
+            //}
+            //print("sum of digits= %d", sum);
+
+
+
+
+
+
+
+
+
+        } 
+    }
+}
